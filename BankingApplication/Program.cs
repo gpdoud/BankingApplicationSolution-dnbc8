8e7026b709b0d70022f680ff5f62d6f228722017
Greg Doud <gpdoud@gmail.com>
2020-02-12 16:36:35 -0500

    Add project files.

---
+++ BankingApplication/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BankingLibrary;
+
+namespace BankingApplication {
+    class Program {
+        static void Main(string[] args) {
+
+            var chk = new Account();
+            chk.Deposit(100);
+            chk.Withdraw(75);
+        
+        }
+    }
+}
---
+++ BankingLibrary/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BankingLibrary {
+
+    public class Account {
+
+        public string AcctNbr { get; set; }
+        public string Description { get; set; }
+        public decimal Balance { get; set; }
+
+        public void Deposit(decimal amount) {
+            Balance += amount;
+        }
+        public void Withdraw(decimal amount) {
+            Balance -= amount;
+        }
+        public void Transfer(decimal amount, Account toAccount, Account fromAccount) {
+            fromAccount.Withdraw(amount);
+            toAccount.Deposit(amount);
+        }
+
+        public Account() { }
+    }
+}
